b392cb54c9bdff82088defc9e5052b2ed1a27ee1
Johnson Mauro <johnsonmauro@gmail.com>
2021-11-02 12:48:22 -0300

    Componentização dos elementos de sidebar e content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from "react";
+
+import { Button } from "../components/Button";
+
+import { api } from "../services/api";
+
+import GenreResponseProps from "./interfaces/GenreResponse";
+
+interface SideBarProps {
+  onClickButton(id: number): void;
+  selected: number;
+}
+
+export function SideBar({ onClickButton, selected }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            id={String(genre.id)}
+            key={genre.id}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickButton(genre.id)}
+            selected={selected === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
